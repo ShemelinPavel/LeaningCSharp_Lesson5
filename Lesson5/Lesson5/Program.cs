d1c51c5c2c58bc118ffa4ed98770362c426c8a10
Pavel Shemelin <pauls0@mail.ru>
2018-11-18 18:42:06 +0300

    набросок task5

--- Lesson5/Lesson5/Program.cs
+++ Lesson5/Lesson5/Program.cs
@@ -1,57 +1,100 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson5
 {
     class Program
     {
-        public struct Student
+        public class Student: IComparer
         {
 
-            public string LastName;
-            public string FirstName;
-            public int Mark1;
-            public int Mark2;
-            public int Mark3;
+            private string lastname;
+            private string firstname;
+            private int[] grades;
 
-            public Student(string lastN, string firstN, int m1, int m2, int m3)
+            public Student(string lastN, string firstN, int[] grArr)
             {
-                LastName = lastN;
-                FirstName = firstN;
+                lastname = lastN;
+                firstname = firstN;
 
-                Mark1 = m1;
-                Mark2 = m2;
-                Mark3 = m3;
+                grades = grArr;
+
+            }
+            public string LastName
+            { get { return this.lastname; } }
+
+            public string FirstName
+            { get { return this.firstname; } }
+
+            public int[] Grades
+            { get { return this.grades; } }
+
+            public double AverageGrade()
+            {
+                return this.grades.Average();
             }
 
-            public double AverageMark()
+            public override string ToString()
             {
-                return (Mark1 + Mark2 + Mark3) / 3;
+                return $"{this.lastname} {this.firstname}";
             }
 
+            public int Compare(object x, object y)
+            {
+
+                try
+                {
+                    Student xStudent = (Student)x;
+                    Student yStudent = (Student)y;
+
+                    return (string.Compare(xStudent.LastName + xStudent.FirstName, yStudent.LastName + yStudent.FirstName));
+                }
+                catch
+                {
+                    throw new NotImplementedException();
+                }
+            }
         }
-
-
 
         static void Main(string[] args)
         {
 
-            SortedList<Student, double> lstStudents = new SortedList<Student, double>();
+            Dictionary<Student, double> lstSt = new Dictionary<Student, double>();
 
-            Student st1 = new Student("Иванов", "Иван", 3, 3, 3);
-            lstStudents.Add(st1, st1.AverageMark());
+            Student st1 = new Student("Иванов", "Иван", new int[]{ 3, 3, 10 });
+            lstSt.Add(st1, st1.AverageGrade());
 
-            Student st2 = new Student("Петров", "Иван", 3, 3, 3);
-            lstStudents.Add(st2, st2.AverageMark());
+            Student st2 = new Student("Петров", "Иван", new int[] { 5, 2, 3 });
+            lstSt.Add(st2, st2.AverageGrade());
+
+            Student st5 = new Student("Петров", "Иван", new int[] { 5, 2, 3 });
+            lstSt.Add(st5, st5.AverageGrade());
 
 
-            IList<double> valList = lstStudents.Values;
+            Student st3 = new Student("Петров", "Иван", new int[] { 3, 3, 3 });
+            lstSt.Add(st3, st3.AverageGrade());
 
+            Student st4 = new Student("Петров", "Иван", new int[] { 30, 60, 3 });
+            lstSt.Add(st4, st4.AverageGrade());
 
+            List<double> l = lstSt.Values.ToList();
+            l.Sort();
 
+            IEnumerable<double> dist = l.Distinct();
+            dist = dist.Take(2);
+
+            foreach (KeyValuePair<Student, double> item in lstSt.OrderBy(key => key.Value))
+            {
+                if (dist.Contains(item.Value))
+                {
+                    Console.WriteLine($"{item.Key} {item.Value}");
+                }
+                else break; // список за пределами нужных значений дальше не интересует
+            }
+
+            Console.ReadKey();
         }
     }
 }
