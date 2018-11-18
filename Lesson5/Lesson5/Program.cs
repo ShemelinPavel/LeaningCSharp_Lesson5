26c6f7b2045da9e427b87bd01720000a595019e3
Pavel Shemelin <pauls0@mail.ru>
2018-11-18 23:41:47 +0300

    task5 доработка прототипа

--- Lesson5/Lesson5/Program.cs
+++ Lesson5/Lesson5/Program.cs
@@ -16,38 +16,44 @@ namespace Lesson5
 
             public Student(string lastN, string firstN, int[] grArr)
             {
-                lastname = lastN;
-                firstname = firstN;
+                LastName = lastN;
+                FirstName = firstN;
 
-                grades = grArr;
+                Grades = grArr;
 
             }
             public string LastName
-            { get { return this.lastname; } }
+            {
+                get { return this.lastname; }
+                set { this.lastname = value; }
+            }
 
             public string FirstName
-            { get { return this.firstname; } }
+            {
+                get { return this.firstname; }
+                set { this.firstname = value; }
+            }
 
             public int[] Grades
-            { get { return this.grades; } }
+            {
+                get { return this.grades; }
+                set { this.grades = value; }
+            }
 
             public double AverageGrade()
             {
                 return this.grades.Average();
             }
 
-            public override string ToString()
-            {
-                return $"{this.lastname} {this.firstname}";
-            }
-
+            #region реализация интерфейса IComparer
             public int Compare(object x, object y)
             {
-
                 try
                 {
                     Student xStudent = (Student)x;
                     Student yStudent = (Student)y;
+
+                    //реализация сравнения не совсен корректная, но мы в задаче по ключу не сортируем - сойдет пока так
 
                     return (string.Compare(xStudent.LastName + xStudent.FirstName, yStudent.LastName + yStudent.FirstName));
                 }
@@ -56,34 +62,73 @@ namespace Lesson5
                     throw new NotImplementedException();
                 }
             }
+            #endregion реализация интерфейса IComparer
+
+            #region переопределяем Equals и GetHashCode для определения дублей
+            public override bool Equals(object obj)
+            {
+                if(obj is Student && obj != null)
+                {
+
+                    Student anotherStud = (Student)obj;
+
+                    if (anotherStud.lastname == this.lastname && anotherStud.firstname == this.firstname)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return (this.lastname + this.firstname).GetHashCode();
+            }
+            #endregion переопределяем Equals и GetHashCode для определения дублей
+
+            #region переопредение представления объекта
+            public override string ToString()
+            {
+                return $"{this.lastname} {this.firstname}";
+            }
+            #endregion переопредение представления объекта
         }
 
         static void Main(string[] args)
         {
 
+            //количество разных значений оценок которые нужно вывести
+            const int countMinGrades = 3;
+
             Dictionary<Student, double> lstSt = new Dictionary<Student, double>();
 
-            Student st1 = new Student("Иванов", "Иван", new int[]{ 3, 3, 10 });
+            Student st1 = new Student("Иванов", "Иван", new int[]{ 3, 3, 3 });
             lstSt.Add(st1, st1.AverageGrade());
 
             Student st2 = new Student("Петров", "Иван", new int[] { 5, 2, 3 });
             lstSt.Add(st2, st2.AverageGrade());
 
-            Student st5 = new Student("Петров", "Иван", new int[] { 5, 2, 3 });
+            Student st5 = new Student("Петров1", "Иван", new int[] { 5, 2, 3 });
             lstSt.Add(st5, st5.AverageGrade());
 
 
-            Student st3 = new Student("Петров", "Иван", new int[] { 3, 3, 3 });
+            Student st3 = new Student("Петров2", "Иван", new int[] { 3, 3, 3 });
             lstSt.Add(st3, st3.AverageGrade());
 
-            Student st4 = new Student("Петров", "Иван", new int[] { 30, 60, 3 });
+            Student st4 = new Student("Петров3", "Иван", new int[] { 30, 60, 3 });
             lstSt.Add(st4, st4.AverageGrade());
 
-            List<double> l = lstSt.Values.ToList();
-            l.Sort();
+            Student st6 = new Student("Петров4", "Иван", new int[] { 30, 60, 30 });
+            lstSt.Add(st6, st6.AverageGrade());
 
-            IEnumerable<double> dist = l.Distinct();
-            dist = dist.Take(2);
+
+            IEnumerable<double> dist = lstSt.Values.ToArray().Distinct();
+            dist.OrderBy(i => i);
+            dist = dist.Take(countMinGrades);
 
             foreach (KeyValuePair<Student, double> item in lstSt.OrderBy(key => key.Value))
             {
