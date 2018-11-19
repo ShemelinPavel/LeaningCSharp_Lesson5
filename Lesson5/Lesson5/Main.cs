4da72c8561a4fe2569a6cd45254cebeb54f6baea
Pavel Shemelin <pauls0@mail.ru>
2018-11-19 20:03:45 +0300

    реализация Main через меню

---
+++ Lesson5/Lesson5/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LeaningCSharp_ClassLibrary;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lesson5
+{
+    class Lesson5
+    {
+
+        static void Main(string[] args)
+        {
+
+            Tasks tasks = new Tasks();
+
+            MainMenu menu = new MainMenu(new string[] { "задача1", "задача2" });
+
+            menu.Show();
+
+            while (true)
+            {
+                ConsoleKeyInfo userChooseKey = Console.ReadKey(true);
+                menu.GotoTask(tasks, userChooseKey.Key);
+
+            }
+
+        }
+    }
+}
